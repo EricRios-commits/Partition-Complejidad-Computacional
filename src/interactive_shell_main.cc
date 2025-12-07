875e3d8de14bc38569c0d3ed9a6906aa733c9afc
luisda <LDRR99100@gmail.com>
2025-12-07 13:15:08 +0000

    Added writer to  interactive_shell_main.cc

--- src/interactive_shell_main.cc
+++ src/interactive_shell_main.cc
@@ -1,5 +1,6 @@
 #include "io/three_d_m_reader.h"
 #include "io/three_d_m_writer.h"
+#include "io/partition_writer.h"
 #include "reduction/three_d_m_to_partition_reducer.h"
 #include <iostream>
 #include <string>
@@ -21,7 +22,7 @@ int main() {
         }
         break;
     }
-    
+
     partition::ThreeDMInstance three_dm_instance = reader.ReadFromFile(filename);
     std::cout << "=== ORIGINAL 3DM INSTANCE ===" << std::endl;
     std::cout << three_dm_instance.ToString() << std::endl;
@@ -30,5 +31,9 @@ int main() {
     std::cout << "\n=== CONVERTED PARTITION INSTANCE ===" << std::endl;
     std::cout << partition_instance.ToString() << std::endl;
 
+    partition::PartitionWriter partition_writer;
+    std::string output = "../instances/3DM/output_partition.txt";
+    std::cout << "=== CONVERTED PARTITION IS PRINTED AT " + output + " ===" << std::endl;
+    partition_writer.WriteToFile(partition_instance, output);
     return 0;
 }
